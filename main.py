ae00fd099c12b345df9b02e56f171f030f5fbdb7
Arya Nazare <arya.nazare29@gmail.com>
2025-11-14 10:00:34 +0530

    changed the format of log messages.

--- main.py
+++ main.py
@@ -244,7 +244,8 @@ async def extract_claims(request: ClaimRequest):
                     if not response.data:
                         raise HTTPException(status_code=400, detail="Insert failed")
                     else:
-                        print(response)
+                        print("Response from DB after inserting claim: ")
+                        print(response.data)
                         print("Inputted claim in DB: "+ claim)
                     # return {"message": "Claim added successfully", "data": response.data[0]}  
                     #Insert clause ends
